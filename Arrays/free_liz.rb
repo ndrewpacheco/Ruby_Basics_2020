7b728e1e8eec9355ed62d6c0988bcf84b61e410f
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-04-23 21:01:55 -0400

    fin hashes

---
+++ Arrays/free_liz.rb
@@ -0,0 +1,14 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+
+my_pets.pop
+
+my_pets << pets[1]
+
+
+
+puts my_pets
+
+# Remove 'lizard' from my_pets then print the value of my_pets.
+
+# Expected output:
